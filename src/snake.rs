c4dfcb40dbcb7a55095bece837efd9b126dedeab
claudia <ceruleancee@gmail.com>
2022-06-04 21:24:38 -0500

    Add snake

--- src/snake.rs
+++ src/snake.rs
@@ -6,19 +6,39 @@ pub fn snake_game(){
 
     pub struct Game {
         // game components
-        graphics:GlGraphics
-        // TODO add snake
+        graphics:GlGraphics,
+        snake: Snake
     }
 
     impl Game{
         // render game components
         pub fn render(&mut self, args: &RenderArgs){
-            self.graphics.draw(args.viewport(), |_context, graphics| {
-                clear(color::NAVY, graphics);
-            // TODO render snake
-        });
+            self.graphics.draw(args.viewport(), |_context, graphics| { clear(color::NAVY, graphics)});
+
+            self.snake.render( &mut self.graphics, args);
+
+        }
     }
 
+    // snake
+    pub struct Snake{
+        pos_x: i32,
+        pos_y: i32
+    }
+
+    impl Snake{
+        // render snake
+        fn render(&mut self, graphics: &mut GlGraphics, args: &RenderArgs){
+            let _red: [f32; 4] = [1.0, 0.0, 0.0, 1.0];
+            let _square = graphics::rectangle::square(self.pos_x as f64,
+                                                     self.pos_y as f64, 20_f64);
+            graphics.draw(args.viewport(),|c,_graphics|{
+                let _transform = c.transform;
+
+                // TODO fix error here
+                //graphics::rectangle(_red, _square, _transform, graphics);
+            });
+        }
     }
 
     // set it up
@@ -33,12 +53,13 @@ pub fn snake_game(){
 
     let mut game = Game{
         graphics:GlGraphics::new(opengl),
+        snake: Snake { pos_x:50, pos_y: 100 }
     };
 
     let mut events = Events::new(EventSettings::new());
     while let Some(e) = events.next(&mut window){
         if let Some(r) = e.render_args(){
-            game.render(&r)
+            game.render(&r);
         }
     }
 
